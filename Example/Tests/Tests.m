1431894e3857ffc97b1c26ff94da3fbc12472a49
Hernan Zalazar <hernanzalazar@gmail.com>
2014-10-31 14:06:00 -0300

    Initial commit

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,57 @@
+// TouchIDAuthTests.m
+//
+// Copyright (c) 2014 Auth0 (http://auth0.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+SpecBegin(InitialSpecs)
+
+describe(@"these will fail", ^{
+
+    it(@"can do maths", ^{
+        expect(1).to.equal(2);
+    });
+
+    it(@"can read", ^{
+        expect(@"number").to.equal(@"string");
+    });
+    
+    it(@"will wait and fail", ^AsyncBlock {
+        
+    });
+});
+
+describe(@"these will pass", ^{
+    
+    it(@"can do maths", ^{
+        expect(1).beLessThan(23);
+    });
+    
+    it(@"can read", ^{
+        expect(@"team").toNot.contain(@"I");
+    });
+    
+    it(@"will wait and succeed", ^AsyncBlock {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+            done();
+        });
+    });
+});
+
+SpecEnd
---
+++ README.md
@@ -0,0 +1,28 @@
+# TouchIDAuth
+
+[![CI Status](http://img.shields.io/travis/Hernan Zalazar/TouchIDAuth.svg?style=flat)](https://travis-ci.org/Hernan Zalazar/TouchIDAuth)
+[![Version](https://img.shields.io/cocoapods/v/TouchIDAuth.svg?style=flat)](http://cocoadocs.org/docsets/TouchIDAuth)
+[![License](https://img.shields.io/cocoapods/l/TouchIDAuth.svg?style=flat)](http://cocoadocs.org/docsets/TouchIDAuth)
+[![Platform](https://img.shields.io/cocoapods/p/TouchIDAuth.svg?style=flat)](http://cocoadocs.org/docsets/TouchIDAuth)
+
+## Usage
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+TouchIDAuth is available through [CocoaPods](http://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+    pod "TouchIDAuth"
+
+## Author
+
+[Auth0](http://auth0.com)
+
+## License
+
+TouchIDAuth is available under the MIT license. See the LICENSE file for more info.
+
