5477f09076a992aee6bb458b9fb2580fc69e230e
Hernan Zalazar <hernanzalazar@gmail.com>
2015-10-30 18:41:44 -0300

    Cleaned old project

--- TouchIDAuthTests/TouchIDAuthTests-Bridging-Header.h
+++ TouchIDAuthTests/TouchIDAuthTests-Bridging-Header.h
@@ -8,11 +8,7 @@
 #import "A0TouchID.h"
 
 @interface A0TouchIDAuthentication (Testing)
-@property (strong, nonatomic) A0TouchID *touchID;
 
-//Not the best way to test it but it's cleaner that handling block stubs
-//TODO: Refactor in step objetcs to mock (maybe NSOperation).
-- (void)checkKeyPair;
-- (void)generateJWT;
+@property (strong, nonatomic) A0TouchID *touchID;
 
 @end
